01baffe1102e9556d9a143218ca3877bbe16a718
Senthil Kumaran <senthil@uthcode.com>
2017-04-14 22:06:28 -0700

    with resources and updated create table.

--- resources/create_table.sql
+++ resources/create_table.sql
@@ -1,9 +1,12 @@
-CREATE TABLE IF NOT EXISTS `golinks`.`UserTable` (
+CREATE TABLE IF NOT EXISTS `golinks`.`LinksTable` (
   `id` INT NOT NULL,
   `username` VARCHAR(45) NULL,
   `userid` BIGINT NULL,
   `shortlink` VARCHAR(45) NULL,
   `longlink` VARCHAR(45) NULL,
+  `hits` BIGINT DEFAULT 0,
+  `created` DATETIME DEFAULT  CURRENT_TIMESTAMP,
+  `updated` DATETIME ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `shortlink_UNIQUE` (`shortlink` ASC))
 ENGINE = InnoDB
